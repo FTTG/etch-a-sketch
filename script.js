ce3bed455ef24cdd9c2e4d16dcf06cad9e8421b5
FTTG <32749931+FTTG@users.noreply.github.com>
2023-04-20 19:36:14 -0600

    Add a slider to change grid size

--- script.js
+++ script.js
@@ -1,24 +1,38 @@
 const container = document.querySelector('.grid-container');
+const gridSize = document.querySelector('#grid-size');
+const sliderText = document.querySelector('#grid-text');
+let pixels = document.querySelectorAll('.pixel');
+
+createGrid();
+
+gridSize.addEventListener("input", createGrid);
 
 // Create the grid inside the grid-container
-for (let i = 0; i < 16; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
-    for (let j = 0; j < 16; j++) {
-        const divs = document.createElement('div');
-        divs.classList.add('pixel');
-        row.appendChild(divs);
+function createGrid() {
+    container.textContent = '';
+    for (let i = 0; i < gridSize.value; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
+        for (let j = 0; j < gridSize.value; j++) {
+            const divs = document.createElement('div');
+            divs.classList.add('pixel');
+            row.appendChild(divs);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
+    sliderText.textContent = `Grid Size: ${gridSize.value}`;
+    // After all pixels are created, update pixels node
+    pixels = document.querySelectorAll('.pixel');
+    paintBlack();
 }
 
-const pixels = document.querySelectorAll('.pixel');
 
-// Give each pixel div the class to paint them black on hover
-for (let pixel of pixels) {
-    pixel.addEventListener('mouseenter', () => pixel.classList.add('black-pixel'));
+function paintBlack() {
+    // Give each pixel div the class to paint them black on hover
+    for (let pixel of pixels) {
+        pixel.addEventListener('mouseenter', () => pixel.classList.add('black-pixel'));
+    }
 }
-
 // Provide the instructions to remove the class to paint them black on use of clear button
 const clearnBtn = document.querySelector('#clear');
 clearnBtn.addEventListener('click', () => {
